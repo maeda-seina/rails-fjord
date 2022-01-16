3c814565cb56282d9bf6c4d3857f01088d9ef2ea
maeda-seina <ayrton19601027@gmail.com>
2022-01-16 22:30:07 +0900

    フォロー機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,5 +22,18 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     byebug
     @users = user.passive_relationships
+    # なぜhas_many :followers, through: :passive_relationships, source: :followingがあるのか調査
+    # →user情報を取れるから便利。passive_relationshipsでは取れない。
+    #
+    # user.passive_relationships
+    # #<ActiveRecord::Associations::CollectionProxy
+    # [#<Follow id: 1, following_id: 51, follower_id: 1, created_at: "2022-01-15 11:36:55.564687000 +0000", updated_at: "2022-01-15 11:36:55.564687000 +0000">
+    # , #<Follow id: 2, following_id: 52, follower_id: 1, created_at: "2022-01-15 11:44:49.706156000 +0000", updated_at: "2022-01-15 11:44:49.706156000 +0000">]>
+    #
+    # user.followers
+    # has_many :followers, through: :passive_relationships, source: :following
+    # #<ActiveRecord::Associations::CollectionProxy
+    # [#<User id: 51, email: "aaa@aaa.com", created_at: "2022-01-15 09:12:54.182702000 +0000", updated_at: "2022-01-15 09:12:54.182702000 +0000", name: "aaa", postal_code: "573-0102", address: "枚方市", s_introduction: "">
+    # , #<User id: 52, email: "bbb@bbb.com", created_at: "2022-01-15 11:44:36.952727000 +0000", updated_at: "2022-01-15 11:44:36.952727000 +0000", name: "bbb", postal_code: "573-0102", address: "枚方市", self_introduction: "">]>
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  # foreign_keyでfollowingモデル(仮想)のidを親のキーとしてもつ??
+
+  #フォローする側のUser（Following）から見て、フォローされる側のUser（Follower）を(中間テーブルを介して)集める。
+  # なので親はfollowing_id(フォローする側)
   has_many :active_relationships, class_name: "Follow", foreign_key: :following_id
+
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
   # followページを作るのに必要。
-  # has_many :followings, through: :active_relationships, source: :follower
+  has_many :followings, through: :active_relationships, source: :follower
   # ========================================================================================
 
   # ====================自分がフォローされるユーザーとの関連 ===================================
-  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  #フォローされる側のUser（Follower）から見て、フォローしてくる側のUser（Following）を(中間テーブルを介して)集める。
+  # なので親はfollower_id(フォローされる側)
   has_many :passive_relationships, class_name: "Follow", foreign_key: :follower_id
   # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
   has_many :followers, through: :passive_relationships, source: :following
